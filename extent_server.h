84e3b7a182676c02fb2d80f109338389020b3fcf
badaro42 <r.bizarra@campus.fct.unl.pt>
2014-09-30 10:15:59 +0100

    Fase2 - extent server feito (nao testado)

--- extent_server.h
+++ extent_server.h
@@ -9,13 +9,24 @@
 
 class extent_server {
 
- public:
-  extent_server();
+public:
+	extent_server();
+	~extent_server();
 
-  int put(extent_protocol::extentid_t id, std::string, int &);
-  int get(extent_protocol::extentid_t id, std::string &);
-  int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
-  int remove(extent_protocol::extentid_t id, int &);
+protected:
+	struct extent_cell {
+		extent_protocol::attr extent_attr;
+		std::string data;
+	};
+
+	std::map<extent_protocol::extentid_t, extent_cell *> ext_storage;
+	pthread_mutex_t storage_mutex;
+
+public:
+	int put(extent_protocol::extentid_t id, std::string, int &);
+	int get(extent_protocol::extentid_t id, std::string &);
+	int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
+	int remove(extent_protocol::extentid_t id, int &);
 };
 
 #endif 
